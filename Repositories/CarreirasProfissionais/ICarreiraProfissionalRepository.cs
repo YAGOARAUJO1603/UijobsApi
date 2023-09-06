799e19073dcaa690d99a8d39553e0a06122758f9
YAGOARAUJO1603 <Y.DESENVOLVEDOR1603@GMAIL.COM>
2023-09-06 20:31:01 -0300

    projeto hmtl subindo para o GitHub

---
+++ Repositories/CarreirasProfissionais/ICarreiraProfissionalRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UIJobsAPI.Models;
+
+namespace UijobsApi.Repositories.CarreirasProfissionais
+{
+    public interface ICarreiraProfissionalRepository
+    {
+        Task<CarreiraProfissional> GetCarreiraProfissionalByIdAsync(int id);
+        Task<CarreiraProfissional> AddCarreiraProfissionalAsync(CarreiraProfissional AddCarreiraProfissional);
+
+        Task<CarreiraProfissional> PutCarreiraProfissionalAsync(int id);
+        Task DeleteCarreiraProfissionalAsync(int id);
+    }
+}
