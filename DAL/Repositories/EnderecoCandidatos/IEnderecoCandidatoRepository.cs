d855da646ede68c488fa62974e2e76a401c670c7
Etec Prof. Horácio Augusto da Silveira <caioquir@gmail.com>
2023-10-17 20:48:21 -0300

    function GetById de EndCand funcionando; Erro no post

--- DAL/Repositories/EnderecoCandidatos/IEnderecoCandidatoRepository.cs
+++ DAL/Repositories/EnderecoCandidatos/IEnderecoCandidatoRepository.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using UIJobsAPI.Data;
 using UIJobsAPI.Models;
 
 namespace UijobsApi.DAL.Repositories.EnderecoCandidatos
